df6673d4310b9d99c86954c6cdbcbe6ca5be329b
Derek White <derekwhite.m@gmail.com>
2026-05-13 14:51:00 -0400

    5. Longest Palindromic String

--- 0005_longest_palindromic_string.py
+++ 0005_longest_palindromic_string.py
@@ -7,7 +7,8 @@ class Solution:
             return s[0]
 
         num_centers = 2 * len(s) - 1
-        longest_palindrome = s[0]
+        best_left = 0
+        best_right = 1
 
         # for each center, expand while chars on each side match
         # start between first and second char
@@ -26,16 +27,19 @@ class Solution:
                 left = char - 1
                 right = char + 1
 
-            best_left = 0
-            best_right = 0
+            current_best_left = 0
+            current_best_right = 0
 
+            # expand out from the center
             while left >= 0 and right < len(s) and s[left] == s[right]:
-                best_left = left
-                best_right = right
+                current_best_left = left
+                current_best_right = right
                 left -= 1
                 right += 1
 
-            if best_right + 1 - best_left > len(longest_palindrome):
-                longest_palindrome = s[best_left : best_right + 1]
+            # compare current to longest palindrome
+            if current_best_right - current_best_left > best_right - best_left:
+                best_left = current_best_left
+                best_right = current_best_right
 
-        return longest_palindrome
+        return s[best_left : best_right + 1]
